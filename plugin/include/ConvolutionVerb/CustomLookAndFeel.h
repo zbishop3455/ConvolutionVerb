7ee152999c1c06f8a21b670fa839db6257f7112b
Zachary Bishop <zbishop3455@gmail.com>
2025-01-03 01:28:29 -0500

    Fixed rotory slider rotation!

--- plugin/include/ConvolutionVerb/CustomLookAndFeel.h
+++ plugin/include/ConvolutionVerb/CustomLookAndFeel.h
@@ -21,20 +21,21 @@ public:
 
     void drawRotarySlider (juce::Graphics& g, int x, int y, int width, int height, float sliderPosProportional, float rotaryStartAngle, float rotaryEndAngle, juce::Slider& slider) override {
         
-        
+        int imageWidth = slider.getBounds().getWidth();
+        int imageHeight = slider.getBounds().getHeight();
+
         juce::Image rotaryImage = juce::ImageCache::getFromMemory(BinaryData::knob_1_png, BinaryData::knob_1_pngSize)
-        .rescaled(width, height, juce::Graphics::highResamplingQuality);
+        .rescaled(imageWidth, imageHeight, juce::Graphics::ResamplingQuality::highResamplingQuality);
 
-        //  rotate the image
+        int centerX = rotaryImage.getWidth() / 2;
+        int centerY = rotaryImage.getHeight() / 2;
+
+        // rotate the image
         float rotation = (rotaryEndAngle - rotaryStartAngle) * sliderPosProportional + rotaryStartAngle;
-        float centreX = width * 0.5f;
-        float centreY = height * 0.5f;
         
-        juce::AffineTransform transform = juce::AffineTransform::rotation(rotation, centreX, centreY);
+        juce::AffineTransform transform = juce::AffineTransform::rotation(rotation, centerX, centerY);
         
-
         g.drawImageTransformed(rotaryImage, transform, false);
-
     }
 
 private:
